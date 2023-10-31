9491ce93ed96c7b9b7e002b8e4816b220980e2a9
hiba <irshadsabri@411@gmail.com>
2023-10-31 04:24:18 -0700

    Done

---
+++ app.js
@@ -0,0 +1,9 @@
+const quizdata * [{
+    question:"what does HTML stand for?",
+    a:"Home Tool Markup language",
+    b:"Hyper links and Text Markup language",
+    c:"Hyper Text Markup Language",
+    d:"Hyper Tool Markup Language",
+    Corrcet:"c",
+}
+]
